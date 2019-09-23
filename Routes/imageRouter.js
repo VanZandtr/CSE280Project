c7ad2b35d6f2a89a2c255f66dc437751e32b6908
Vanzandtr <rlv220@lehigh.edu>
2019-09-23 15:31:26 -0400

    wrote code to use compression py, will test later

--- Routes/imageRouter.js
+++ Routes/imageRouter.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var Image = require('../models/imageModel');
 var mongoose = require('mongoose')
-//const formidable = require('formidable');
+
+const fs = require('fs');
 const path = require('path');
 const uploadDir = ''; // uploading the file to the same path as app.js
 
@@ -27,6 +28,27 @@ imageRouter.use('/:imageId', (req, res, next)=>{
 })
 imageRouter.route('/:imageId')
     .get((req, res) => {
+        //--------------------------------
+        //write req.image.imageData to file
+        fs.writeFile(req.image.imageName + ".txt", req.image.imageData, (err) => {
+            if(err)
+                res.status(500).send(err);
+        });
+
+        //spawn child process
+        var spawn = require("child_process").spawn;
+
+        //have process execute convert map.txt (compressed) -------> map.bmp (decompressed)
+        var process = spawn('python', ["./map_compress_decompress.py",
+                                req.image.imageName,
+                                req.image.imageName]);
+        
+        //send image                                
+        process.stdout.on('data', function(data){
+            res.send(data);
+        })                                
+        //--------------------------------
+
         //send the found image.data to the user
         res.send(req.image.imageData);
         //res.json(req.image);
