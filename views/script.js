eacd16c716d2b576ad2c20b15da730c98bd4b8da
VanZandtr <40271531+VanZandtr@users.noreply.github.com>
2019-10-31 16:49:38 -0400

    Testing image append methods

--- views/script.js
+++ views/script.js
@@ -76,7 +76,7 @@
   document.querySelector("#buildingdest").addEventListener('click', function(e) {e.stopPropagation()});
   
  $("#Go").click(function() {
-   var img = ("<img width=\"78px\" src = https://cse280project.herokuapp.com/image/2961b60d28a39e7969eace5fa14abff8.png");
+   var img = $('<img />' {src : 'https://cse280project.herokuapp.com/image/2961b60d28a39e7969eace5fa14abff8.png'});
    //var img = $('<img />', {src : 'http://webpage.com/images/' + $('#imagename').val() +'.png'});
    //img.appendTo('mapoverlay');
    $("#mapoverlay").append(img);
